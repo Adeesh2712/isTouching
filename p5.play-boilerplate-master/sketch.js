d463fd097a3023b1ff9e4eb8037971160c4cd616
Adeesh2712 <76524701+Adeesh2712@users.noreply.github.com>
2021-01-18 19:41:13 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,35 @@
+var fixedRect;
+var movingRect;
+
+
+
+function setup() {
+  createCanvas(1200,800);
+
+  fixedRect = createSprite(400, 200, 50, 50);
+
+  movingRect = createSprite(200,200,40,10);
+}
+
+function draw() {
+  background(0);  
+
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+
+if(movingRect.x-fixedRect.x < fixedRect.width/2+movingRect.width/2
+  &&fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2
+  &&fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2
+  &&movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.height/2)
+{
+
+  movingRect.shapeColor="red";
+  fixedRect.shapeColor="red";
+}
+else{
+  movingRect.shapeColor="yellow";
+  fixedRect.shapeColor="yellow";
+}
+
+  drawSprites();
+}
